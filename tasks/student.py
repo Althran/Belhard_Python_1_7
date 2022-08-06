25be80d4f46cb77352205ab56a22277dd4ad366b
Althran <overlord106@gmail.com>
2022-08-06 21:36:19 +0300

    Complete student

--- tasks/student.py
+++ tasks/student.py
@@ -19,3 +19,48 @@
 
 Вывести студентов, у которых средний балл больше 5
 """
+
+
+class Student:
+
+    def __init__(self, surname, name, group, average_score):
+        self.name = name
+        self.surname = surname
+        self.group = group
+        self.average_score = average_score
+
+    def __eq__(self, other):
+        return self.average_score == other
+
+    def __ne__(self, other):
+        return self.average_score != other
+
+    def __lt__(self, other):
+        return self.average_score < other
+
+    def __gt__(self, other):
+        return self.average_score > other
+
+    def __le__(self, other):
+        return self.average_score <= other
+
+    def __ge__(self, other):
+        return self.average_score >= other
+
+    def __repr__(self):
+        return '{' + self.surname + ', ' + self.name + ', ' + str(self.group) + ', ' + str(self.average_score) + '}'
+
+
+students = [Student(surname='VI', name='lui', group=1, average_score=6),
+            Student(surname='VII', name='lu', group=1, average_score=4),
+            Student(surname='VP', name='ui', group=1, average_score=7),
+            Student(surname='VW', name='i', group=2, average_score=1),
+            Student(surname='VQ', name='loop', group=3, average_score=5)]
+
+
+students1 = sorted(students)
+students_reverse = sorted(students, reverse=True)
+print(students1)
+print(students_reverse)
+students2 = list(filter(lambda x: (x > 5), students1))
+print(students2)
